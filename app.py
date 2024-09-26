2860e5b4b4303d6a8cb647c273e00649815709eb
Nati Lintzer <nlinzer@gmail.com>
2024-09-26 17:53:17 +0300

    add seed function to app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from controller.mission_controller import mission_bluprint
-from repository.
 from flask import Flask
-
+from repository.seed_repository import seed
 
 app = Flask(__name__)
 
 if __name__ == '__main__':
+    seed()
     app.register_blueprint(mission_bluprint, url_prefix="/api/mission")
     app.run(debug=True)
